b5962fec3b6199539b9bd3d7de7da93b6cd77411
lucas-barake <86905052+lucas-barake@users.noreply.github.com>
2024-12-01 10:39:14 -0500

    chore: remove SpansExporterLive

--- src/lib/app-runtime.ts
+++ src/lib/app-runtime.ts
@@ -1,19 +1,6 @@
 import { Layer, Logger, ManagedRuntime } from "effect";
-import { type Configuration, layer as webSdkLayer } from "@effect/opentelemetry/WebSdk";
-import { BatchSpanProcessor } from "@opentelemetry/sdk-trace-web";
-import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http";
+import { NetworkMonitor } from "@/lib/services/network-monitor.ts";
 
-const SpansExporterLive = webSdkLayer((): Configuration => {
-  return {
-    resource: {
-      serviceName: "my-service",
-    },
-    spanProcessor: new BatchSpanProcessor(
-      new OTLPTraceExporter({
-        url: "http://localhost:4318/v1/traces",
-      }),
-    ),
-  };
-});
-
-export const AppRuntime = ManagedRuntime.make(Layer.mergeAll(SpansExporterLive, Logger.pretty));
+export const AppRuntime = ManagedRuntime.make(
+  Layer.mergeAll(Logger.pretty, NetworkMonitor.Default),
+);
